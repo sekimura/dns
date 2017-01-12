f42b8cae8f8ddbdac5d212a8f0b85821c7ddffa2
Masayoshi Sekimura <sekimura@gmail.com>
2017-01-11 23:10:06 -0800

    Add comments to decompress
    Also tweak offset handling a bit to reduce redundancy

--- decompress.go
+++ decompress.go
@@ -5,26 +5,27 @@ import (
 	"encoding/binary"
 )
 
+// decompress implements RFC 1035: 4.1.4. Message compression"
+// which follows the pointer of offsets until it finds 0x0.
+// It returns labels as string and length of bytes read as int
 func decompress(b []byte, off int) (string, int) {
 	buf := bytes.NewBuffer(nil)
 	off0 := off
 	for {
 		c := b[off]
+		off++
 		if c >= 0xc0 { // TODO: handle 01 and 10 bits cases
 			// technically offset is uint14 value
 			// But message won't be longer than 512...
-			offset := binary.BigEndian.Uint16([]byte{c ^ 0xc0, b[off+1]})
-			off++
+			offset := binary.BigEndian.Uint16([]byte{c ^ 0xc0, b[off]})
 			s, _ := decompress(b, int(offset))
 			buf.WriteString(s)
 			break
 		} else {
 			if c == 0x0 {
-				off++
 				break
 			}
-			l := int(b[off])
-			off++
+			l := int(c)
 			buf.Write(b[off : off+l])
 			off += l
 			buf.WriteString(".")
